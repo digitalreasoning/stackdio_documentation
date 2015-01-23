270e3855bb61ae763808f156c01f6541e9264da7
Sean Story <sean.story@digitalreasoning.com>
2015-01-23 16:02:21 -0600

    changed theme to agogo

--- source/conf.py
+++ source/conf.py
@@ -105,7 +105,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 """
-html_theme = 'pyramid'
+html_theme = 'agogo'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
